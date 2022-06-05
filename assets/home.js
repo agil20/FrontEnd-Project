b51913055a06ea82e5c3fb54f2ea8fc941f7f070
agil20 <100156146+agil20@users.noreply.github.com>
2022-06-05 07:55:26 -0700

    feat: modala click methodunu duzeltdim

--- assets/home.js
+++ assets/home.js
@@ -1,9 +1,9 @@
 let currency=document.getElementById("currency");
 let language=document.getElementById("language");
 let eng=document.getElementById("eng");
-let delivery_icon=document.getElementsByClassName("delivery-icon")
 let valyuta=document.getElementById("valyuta")
 let my_modal=document.getElementsByClassName("my-modal")
+
 language.addEventListener("mouseenter", function(){
 eng.classList.remove("none")
 });
@@ -21,8 +21,8 @@ language.addEventListener("mouseleave", function(){
 currency.addEventListener("mouseleave", function(){
     valyuta.classList.add("none")
 });
-delivery_icon.addEventListener("click",function(){
-})
-dmy_modal_icon.addEventListener("click",function(){
+my_modal.addEventListener("click",function(e){
+alert("lhf")
+
 
 })

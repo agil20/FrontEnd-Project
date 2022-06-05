bf09409cb0ccd9c6c64ed33e7f9040f6ecced92c
agil20 <100156146+agil20@users.noreply.github.com>
2022-06-05 07:44:50 -0700

    feat" modalin icindeki divi hell etdim

--- assets/home.js
+++ assets/home.js
@@ -1,7 +1,9 @@
 let currency=document.getElementById("currency");
 let language=document.getElementById("language");
 let eng=document.getElementById("eng");
+let delivery_icon=document.getElementsByClassName("delivery-icon")
 let valyuta=document.getElementById("valyuta")
+let my_modal=document.getElementsByClassName("my-modal")
 language.addEventListener("mouseenter", function(){
 eng.classList.remove("none")
 });
@@ -19,3 +21,8 @@ language.addEventListener("mouseleave", function(){
 currency.addEventListener("mouseleave", function(){
     valyuta.classList.add("none")
 });
+delivery_icon.addEventListener("click",function(){
+})
+dmy_modal_icon.addEventListener("click",function(){
+
+})
